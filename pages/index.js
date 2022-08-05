6ea87d9be5b3e196d8c23867c6400074e856e2c6
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-08-05 23:51:20 +0530

    c

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const Home = () => {
   const [nf, setnf] = useState(false);
 
   const octokit = new Octokit({
-    auth: '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'
+    auth: process.env.GIT_TOKEN
   })
 
   const handleChange = (e) => {
@@ -51,7 +51,7 @@ const Home = () => {
     setdata(res.data)
     setready(true)}
     catch(err){
-      document.getElementById("error").innerHTML = "No user found";
+      setnf(true)
     }
 
   }
@@ -67,13 +67,13 @@ const Home = () => {
           <img src="https://img.icons8.com/clouds/100/000000/github.png" />
           <h1 className='text-[4vh]' style={{ margin: "0vh" }}>Repos Finder</h1>
         </div>
-        <div className='md:my-0 my-[6vh]'>
+        <div className='md:my-0 flex my-[6vh]'>
           <input className='px-[2vh] py-[1vh] rounded-lg' value={username} placeholder='enter the usename' onChange={(e) => handleChange(e)}></input>
           <button className='hover:opacity-90 text-black  bg-[#c7ede6] rounded-lg h-[5.57vh] w-[15vh]  ml-[5vh] ' onClick={search}>search</button></div>
       </div>
       
 
-      {ready ? <div id='error'> <div className=' px-[4vh]  py-[5vh] h-[40vh] flex flex-col md:flex-row  md:space-x-[16vh]' >
+      {ready ? <div> <div className=' px-[4vh]  py-[5vh] h-[40vh] flex flex-col md:flex-row  md:space-x-[16vh]' >
         <div className='flex flex-col justify-center items-center '>
           <img className='w-[30vh]  border-2 p-[0.5vh] rounded-full drop-shadow-xl' src={user.avatar_url}></img>
           <div className='flex mt-[2vh] ' ><img className='mr-[1vh]' src="https://img.icons8.com/material-rounded/24/22C3E6/link--v1.png" /><a className='truncate' href={user.html_url}>{user.html_url}</a></div>
@@ -102,7 +102,7 @@ const Home = () => {
 
         <Box className='fixed top-[50%] left-[50%]' sx={{ display: 'flex' }}>
           <CircularProgress />
-        </Box> : ""}
+        </Box> : nf?<div>No user found</div>:""}
 
 
 
